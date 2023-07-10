cd8e95973412a019d2d4af367e9fe25266a6e36f
Onkar Mishra <onkarmishra@Onkars-Air.attlocal.net>
2023-07-10 18:32:12 -0500

    first commit

---
+++ src/test/java/com/openshift/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openshift.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
